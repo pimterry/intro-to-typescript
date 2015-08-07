d6c258363985824892347f687cbbb925dda5caaf
Tim Perry <tim.perry@softwire.com>
2015-08-07 12:52:41 +0100

    Initial generator-bespoke setup

---
+++ src/scripts/main.js
@@ -0,0 +1,28 @@
+// Require Node modules in the browser thanks to Browserify: http://browserify.org
+var bespoke = require('bespoke'),
+  cube = require('bespoke-theme-cube'),
+  keys = require('bespoke-keys'),
+  touch = require('bespoke-touch'),
+  pdf = require('bespoke-pdf'),
+  bullets = require('bespoke-bullets'),
+  backdrop = require('bespoke-backdrop'),
+  scale = require('bespoke-scale'),
+  hash = require('bespoke-hash');
+
+// Bespoke.js
+bespoke.from('article', [
+  cube(),
+  keys(),
+  touch(),
+  pdf(),
+  bullets('li, .bullet'),
+  backdrop(),
+  scale(),
+  hash()
+]);
+
+// Prism syntax highlighting
+// This is actually loaded from "bower_components" thanks to
+// debowerify: https://github.com/eugeneware/debowerify
+require('prism');
+
