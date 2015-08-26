8b65f1e0d7ea1cd0b69c44bab98ee7eeec059bbd
Tim Perry <tim.perry@softwire.com>
2015-08-26 17:12:38 +0100

    Flesh out notes and finish up ready for talk

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -6,7 +6,8 @@ var bespoke = require('bespoke'),
   bullets = require('bespoke-bullets'),
   backdrop = require('bespoke-backdrop'),
   scale = require('bespoke-scale'),
-  hash = require('bespoke-hash');
+  hash = require('bespoke-hash'),
+  notes = require('./bespoke-notes');
 
 // Bespoke.js
 bespoke.from('article', [
@@ -16,7 +17,8 @@ bespoke.from('article', [
   bullets('.bullet > li'),
   backdrop(),
   scale(),
-  hash()
+  hash(),
+  notes()
 ]);
 
 // Prism syntax highlighting
