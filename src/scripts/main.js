b059a8de650548785767ad223bf93be23962b2f9
Tim Perry <pimterry@gmail.com>
2015-11-10 18:21:48 +0100

    Remove themes and cubes, pair down to pure built-in styles (for perf and simplicity)

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,21 +1,21 @@
 // Require Node modules in the browser thanks to Browserify: http://browserify.org
 var bespoke = require('bespoke'),
-  cube = require('bespoke-theme-cube'),
+  classes = require('bespoke-classes'),
   keys = require('bespoke-keys'),
   touch = require('bespoke-touch'),
   bullets = require('bespoke-bullets'),
-  backdrop = require('bespoke-backdrop'),
+  state = require('bespoke-state'),
   scale = require('bespoke-scale'),
   hash = require('bespoke-hash'),
   notes = require('./bespoke-notes');
 
 // Bespoke.js
 bespoke.from('article', [
-  cube(),
+  classes(),
   keys(),
   touch(),
   bullets('.bullet > li'),
-  backdrop(),
+  state(),
   scale(),
   hash(),
   notes()
